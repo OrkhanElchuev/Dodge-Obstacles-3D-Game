60d96f3bb836d3999b9e628cbf110f0241f1ecaf
OrkhanElchuev <orkhan.elchuev@gmail.com>
2026-01-10 22:32:51 +0100

    Add Player Movement Controls

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 
 public class MovePlayer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] float moveSpeed = 10f;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        float yValue = 0f;
+
+        transform.Translate(xValue, yValue, zValue);
     }
 }
